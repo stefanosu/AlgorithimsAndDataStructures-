a1a6c44a6a89f60bc372570bfa979afd4e946725
Stefanos <stefanosu422@gmail.com>
2019-10-30 19:55:02 -0400

    first of many algorithm problems

---
+++ index.js
@@ -0,0 +1,30 @@
+////First Problem////
+
+///Frequency Counter -validAnagram Problem: 
+
+//1. Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is word, name, or phrase
+///formed by rearranging the letters of another, such as cinema, formed from iceman. 
+
+
+function validAnagram(arr1, arr2){
+    if(arr1.length !== arr2.length){
+        return false; 
+    }
+    let anagramCount1 = {}; 
+    let anagramCount2 = {}; 
+    for(let val of arr1){
+        anagramCount1[val] = (anagramCount1[val] || 0) + 1;  
+    }
+    for(let val of arr2){
+        anagramCount2[val] = (anagramCount2[val] ||0) + 1; 
+    }
+    for(let key in anagramCount1) {
+        if(!(key in anagramCount2)) {
+            return false;
+        }
+        if(anagramCount2[key] !== anagramCount1[key]){
+            return false;
+        }
+    }
+    return true;    
+}
