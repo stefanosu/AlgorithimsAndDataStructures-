30c844726d99374c36348968361177ca2f782eb8
Stefanos <stefanosu422@gmail.com>
2019-10-31 09:08:30 -0400

    Update Index.js

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 
 //1. Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is word, name, or phrase
 ///formed by rearranging the letters of another, such as cinema, formed from iceman. 
-
+//Time Complexity is 0(N) linear 
+//Space Complexity is 0(1) constant 
 
 function validAnagram(arr1, arr2){
     if(arr1.length !== arr2.length){
