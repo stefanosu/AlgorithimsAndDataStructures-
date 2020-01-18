1dfe3742642d13abc4dae5fc847ab86bbefa379a
Stefanos <stefanosu422@gmail.com>
2020-01-18 17:39:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -151,3 +151,22 @@ const validAnagram = (arr1, arr2) => {
     //Space Complexity O(1)Constant 
 
 
+
+const binarySearch = (arr, val) => {
+    var start = 0;
+    var end = arr.length - 1;
+    var middle = Math.floor((start + end) / 2);
+    while(arr[middle] !== val && start <= end) {
+        if(val < arr[middle]) end = middle - 1;
+        else start = middle + 1;
+        middle = Math.floor((start + end) / 2);
+    }
+    return arr[middle] == val ? middle : -1;
+};
+
+binarySearch([2,5,6,9,13,15,28,30], 103);
+//Time Complexity log(N)
+//Space Complexity 0(1)
+
+
+
